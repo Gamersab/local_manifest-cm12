12abc041aa68d9d8ee3a6dd9c8e7dd4a223bfbb1
Abdul Raffay <abdulraffay502@gmail.com>
2014-12-13 21:18:29 +0500

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -19,23 +19,8 @@ cherries+=(79582)
 # Build : Add back support for msm7x27a Board
 cherries+=(79581)
 
-# frameworks/opt/telephony : UiccController: Query GET_SIM_STATUS when radio state is ON
-cherries+=(79187)
-
-# DCTracker: HACK Fix eHRPD/LTE data connection.
-cherries+=(79749)
-
-# Telephony: DcTracker: Fix CDMA APN Data issues.
-cherries+=(79764)
-
-# Implement fakeiccid for legacy rils (1/2)
-#cherries+=(79835)
-
-# CardStateMonitor: Use fakeiccid if needed for legacy ril (2/2)
-#cherries+=(79835)
-
-# Legacy RIL requires radio technology equals CDMA or GSM
-cherries+=(79912)
+# Correctly provide the technology to setup data connectiion
+cherries+=(81082)
 
 # recovery: Always include ext4 stuff
 cherries+=(79803)
@@ -43,5 +28,7 @@ cherries+=(79803)
 # build: Add option to disable block-based ota
 cherries+=(78849)
 
-${android}/build/tools/repopick.py -b ${cherries[@]}
+# Revert "Revert "Reenable support for non-PIE executables""
+cherries+=(79136)
 
+${android}/build/tools/repopick.py -b ${cherries[@]}
